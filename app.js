ae477c8c6f579727e4f4aadefcc68fc6590a8c79
Buzzgrow <artur_pg2011@hotmail.com>
2020-11-01 08:53:30 -0300

    trying to realize

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  console.log("Server Running!");
+  res.json({message: 'Hello, world!'});
+});
+
+app.listen(8080);
