8ea059f49af7eb19f34ab8e25ccbb8fd5c4c8d1c
Buzzgrow <artur_pg2011@hotmail.com>
2020-11-03 16:54:00 -0300

    exporting cargos routes

--- src/routes/cargos.js
+++ src/routes/cargos.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
+
 const cargoControllers = require('../controllers/cargos.js');
 
 router.get('/', cargoControllers.fetchAll);
 router.post('/', cargoControllers.createOne);
 router.delete('/:idCargo', cargoControllers.deleteById);
+
+module.exports = router;
