c7383c2196dce850e83e246f8bb8d0c491ca88eb
Artur Freire <freirart.contato@gmail.com>
2020-11-04 09:17:07 -0300

    adding cargo put route

--- src/routes/cargos.js
+++ src/routes/cargos.js
@@ -6,6 +6,7 @@ const cargoControllers = require('../controllers/cargos.js');
 
 router.get('/', cargoControllers.fetchAll);
 router.post('/', cargoControllers.createOne);
+router.put('/edit/', cargoControllers.updateCargo);
 router.delete('/delete/:idCargo', cargoControllers.deleteById);
 
 module.exports = router;
