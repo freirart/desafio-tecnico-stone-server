39199f665fc3dd7ed1dfde961f38fe6671149139
Artur Freire <freirart.contato@gmail.com>
2020-11-04 08:51:19 -0300

    updating route url

--- src/routes/cargos.js
+++ src/routes/cargos.js
@@ -6,6 +6,6 @@ const cargoControllers = require('../controllers/cargos.js');
 
 router.get('/', cargoControllers.fetchAll);
 router.post('/', cargoControllers.createOne);
-router.delete('/:idCargo', cargoControllers.deleteById);
+router.delete('/delete/:idCargo', cargoControllers.deleteById);
 
 module.exports = router;
